2659f87d8c40e552a0abf385b24ccb3d7d7ac295
Bilqisu Ismail <ismail.bilkisu@gmail.com>
2023-05-15 15:48:14 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import ShowMe from "./ShowMe"
+
+// parent component
+function App() {
+  return (
+    <div className='coker'>
+       {/* child component */}
+    <ShowMe />
+
+    </div>
+  )
+  
+}
+
+export default App
